243d71cc4f83a230d413950c9c7668d10906f35c
nakakouchi <61769004+nakakouchi@users.noreply.github.com>
2021-01-25 21:12:08 +0900

    課題提出2-1

---
+++ JavaScript/2-1/main.js
@@ -0,0 +1,7 @@
+function changeColor(){
+  document.getElementById('box').style.backgroundColor = 'red';
+}
+
+function revertColor(){
+  document.getElementById('box').style.backgroundColor = null;
+}
